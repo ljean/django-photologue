20319f50860cc1cb117d0e82297640d2c6d2453f
Luc Jean <ljean@apidev.fr>
2014-03-20 16:23:01 +0100

    Fix for min height of the slideshow

--- photologue/templatetags/pl_extra_tags.py
+++ photologue/templatetags/pl_extra_tags.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 from django import template
 from django.template.base import Variable, VariableDoesNotExist
+import os.path
+import unicodedata
+try:
+    from PIL import Image
+except ImportError:
+    import Image
 
 register = template.Library()
 
 @register.filter
 def first_in_gallery(photo, gallery):
     return (None == photo.get_previous_in_gallery(gallery))
+
+@register.filter
+def max_height(gallery, size="display"):
+    max_val = 0
+    for photo in gallery.photos.all():
+        photo_filename = getattr(photo, "get_{0}_filename".format(size))()
+        img = Image.open(photo_filename)
+        (w, h) = img.size
+        max_val = max(max_val, h)
+    return max_val
+    
+@register.filter
+def basename(fullname):
+    return os.path.basename(fullname)
+
+@register.filter
+def normalize_utf8_to_ascii(ustr):
+    try:
+        return unicodedata.normalize('NFKD', ustr).encode('ascii','ignore')
+    except TypeError:
+        return ustr
