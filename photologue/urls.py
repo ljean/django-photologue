e0a61ac2c21b34c1c00fb795837627708ebd65fe
Luc Jean <ljean@apidev.fr>
2016-07-07 10:48:55 +0200

    Fix warning

--- photologue/urls.py
+++ photologue/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
         GalleryArchiveIndexView.as_view(),
         name='pl-gallery-archive'),
     url(r'^$',
-        RedirectView.as_view(url=reverse_lazy('pl-gallery-archive')),
+        RedirectView.as_view(url=reverse_lazy('pl-gallery-archive'), permanent=False),
         name='pl-photologue-root'),
-    url(r'^gallery/(?P<slug>[\-\d\w]+)/$', GalleryDetailView.as_view() , name='pl-gallery'),
+    url(r'^gallery/(?P<slug>[\-\d\w]+)/$', GalleryDetailView.as_view(), name='pl-gallery'),
     url(r'^gallery/page/(?P<page>[0-9]+)/$', GalleryListView.as_view(), name='pl-gallery-list'),
 
     url(r'^photo/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\-\d\w]+)/$',
